db2b7a0f97cf703f2f2a57a6b7b63ff79c54a820
jucaboris <120645398+jucaboris@users.noreply.github.com>
2026-03-08 19:20:54 -0300

    Refactor Firebase imports and configuration

--- db.js
+++ db.js
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "";
+import { getDatabase, ref, push, onValue, set, onChildAdded, remove } from "";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBU61OITCzDbxC_Uyxn_CL4MOZAdUaZXSM",
-  authDomain: "sky-authority.firebaseapp.com",
-  databaseURL: "https://sky-authority-default-rtdb.firebaseio.com",
-  projectId: "sky-authority",
-  storageBucket: "sky-authority.firebasestorage.app",
-  messagingSenderId: "948867971899",
-  appId: "1:948867971899:web:7b09cc8e1471abed51151e"
+apiKey: "AIzaSyBU61OITCzDbxC_Uyxn_CL4MOZAdUaZXSM",
+authDomain: "sky-authority.firebaseapp.com",
+databaseURL: "",
+projectId: "sky-authority",
+storageBucket: "sky-authority.firebasestorage.app",
+messagingSenderId: "948867971899",
+appId: "1:948867971899:web:7b09cc8e1471abed51151e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export { ref, push, onValue, set, onChildAdded, remove };
