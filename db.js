e9fdbb3e547b160e841ef6e0bd58b924b6962b39
jucaboris <120645398+jucaboris@users.noreply.github.com>
2026-03-08 19:14:01 -0300

    Initialize Firebase in db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBU61OITCzDbxC_Uyxn_CL4MOZAdUaZXSM",
+  authDomain: "sky-authority.firebaseapp.com",
+  databaseURL: "https://sky-authority-default-rtdb.firebaseio.com",
+  projectId: "sky-authority",
+  storageBucket: "sky-authority.firebasestorage.app",
+  messagingSenderId: "948867971899",
+  appId: "1:948867971899:web:7b09cc8e1471abed51151e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
